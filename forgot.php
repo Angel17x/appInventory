7b2c88cda2a666088c8b8f855232106bc7450dd4
Angel Lugo <angel26078613@gmail.com>
2020-07-04 21:19:22 -0400

    eh agregado un .gitignore

---
+++ forgot.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <div class="contenedor">
+   <h1>Recuperacion de Contraseña</h1>
+   <p>Lorem, ipsum dolor sit amet consectetur 
+       adipisicing elit. Repellendus cupiditate 
+       repudiandae ab consectetur, excepturi natus 
+       cum eos officiis optio atque magnam perspiciatis 
+       saepe expedita architecto ipsum ullam illum
+        dolore! Aliquid minus nam obcaecati voluptates!
+         Amet excepturi magni culpa nisi id.</p>
+   </div>
+</body>
+</html>
